885b1ad3de5f429f7beeb3399da99e871c829bfa
Homkers <79154848+Homkers@users.noreply.github.com>
2023-09-14 17:05:55 +0300

    Delete python bot.py

--- python bot.py
+++
@@ -1,6 +0,0 @@
-import telebot
-bot = telebot.TeleBot('1658937169:AAFTk2lFb8emYoRvqTcpAmCUePMg22S_bzo')
-@bot.message_handler(content_types=['text'])
-def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
-bot.polling(none_stop=True, interval=0)
